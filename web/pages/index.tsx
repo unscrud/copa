2cc8af6f504d0ae3a6d8324fa1f175a7b28e4f68
Daniel Carvalho <daniel.devop@gmail.com>
2022-11-06 12:29:24 -0300

    trazendo a contagem de pools

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
+
+  fetch('http://localhost:3333/pools/count')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+
   return (
-    <h1>Olá Mundo</h1>
+    <h1>React</h1>
   )
 }
