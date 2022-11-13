182707331e1ae15685c1ee3ebaeb042f6563c6f1
Daniel Carvalho <daniel.devop@gmail.com>
2022-11-13 08:23:41 -0300

    tela de signin

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "native-base";
+
+export function SignIn (){
+  return (
+    <Center 
+    flex={1}
+    bgColor='gray.900'
+    >
+      <Text color='yellow.300' fontSize='30'>SIGNIN!</Text>
+    </Center>
+  )
+}
