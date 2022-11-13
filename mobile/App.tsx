18e79e0ee2225815da9a543f93f95fb722a77d3f
Daniel Carvalho <daniel.devop@gmail.com>
2022-11-13 08:25:04 -0300

    uso de fontes e indicação de carregamento

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, VStack, Text } from "native-base";
+import { NativeBaseProvider } from "native-base";
 import { THEME } from './src/styles/theme';
+import { 
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+ } from "@expo-google-fonts/roboto";
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/Signin';
 
 export default function App() {
+  const[fontsLoaded]=useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <VStack 
-        flex={1}
-        bgColor='gray.900'
-        alignItems="center"
-        justifyContent='center'
-      >
-        <Text color='yellow.300' fontSize='30'>Olá Mundo</Text>
-        <StatusBar style="auto" />
-      </VStack>
+        { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
