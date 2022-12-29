b9591633bcf35e1de9cde07b4b0017064582412d
Daniel Carvalho <daniel.devop@gmail.com>
2022-12-29 16:50:07 -0300

    texto em h1

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,13 +5,17 @@
 import Image from 'next/image'
 import appPreviewImg from '../assets/app-copa-preview.png'
 import logoImg from '../assets/logo.svg'
+
+
 export default function Home(/*props: HomeProps*/) {
 
   return (
     // <h1 className="text-violet-500 font-bold text-4xl">Contagem: {props.count}</h1>
     <div>
       <main>
-        <Image src={logoImg} alt="NLW Copa"></Image>
+        <Image src={logoImg} alt="NLW Copa" />
+
+        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
       </main>
       
       <Image src={appPreviewImg} alt="Dois celulares exibindo uma prévia da aplicação." />
