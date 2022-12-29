98469ee1a40e77117517433f685f7e1c79fc2303
Daniel Carvalho <daniel.devop@gmail.com>
2022-12-29 20:59:36 -0300

    parte inferior da primeira coluna

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import appPreviewImg from '../assets/app-copa-preview.png'
 import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImage from '../assets/icon-check.png'
 
 export default function Home(/*props: HomeProps*/) {
 
@@ -18,13 +19,13 @@ export default function Home(/*props: HomeProps*/) {
         <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
 
         <div>
-          <Image src={usersAvatarExampleImg} />
+          <Image src={usersAvatarExampleImg} alt="" />
 
           <strong>
             <span>+12.592</span> pessoas já estão usando
           </strong>
         </div>
-
+ 
         <form>
           <input type="text" required placeholder='Qual nome do seu bolão?' />
           <button type='submit'>Criar meu bolão</button>
@@ -32,6 +33,22 @@ export default function Home(/*props: HomeProps*/) {
 
         <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
       
+        <div>
+          <div>
+            <Image src={iconCheckImage} alt=""/>
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImage} alt=""/>
+            <div>
+              <span>+192.847</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
       </main>
       
       <Image src={appPreviewImg} alt="Dois celulares exibindo uma prévia da aplicação." />
