5cb9c40e6801fe378fae20bc9df5247c2c835acd
Daniel Carvalho <daniel.devop@gmail.com>
2022-12-29 16:52:34 -0300

    imagem dos avatares

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,7 +5,7 @@
 import Image from 'next/image'
 import appPreviewImg from '../assets/app-copa-preview.png'
 import logoImg from '../assets/logo.svg'
-
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 
 export default function Home(/*props: HomeProps*/) {
 
@@ -16,6 +16,11 @@ export default function Home(/*props: HomeProps*/) {
         <Image src={logoImg} alt="NLW Copa" />
 
         <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} />
+          
+        </div>
       </main>
       
       <Image src={appPreviewImg} alt="Dois celulares exibindo uma prévia da aplicação." />
