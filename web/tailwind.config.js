a8c78ee6f11b28f57de7e102523587ffae8c8861
Daniel Carvalho <daniel.devop@gmail.com>
2022-12-30 12:18:22 -0300

    imagem de fundo

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
+
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
+
       colors: {
         ignite: {
           500: '#129E57'
