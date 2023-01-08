1131598db38ee58156d855987617f5672f390eca
Daniel Carvalho <daniel.devop@gmail.com>
2023-01-08 07:33:32 -0300

    Importando um svg

--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,12 +1,14 @@
 import { Center, Text } from "native-base";
 
+import Logo from "../assets/logo.svg";
+
 export function SignIn (){
   return (
     <Center 
-    flex={1}
-    bgColor='gray.900'
+      flex={1}
+      bgColor='gray.900'
     >
-      <Text color='yellow.300' fontSize='30'>SIGNIN!</Text>
+      <Logo />  
     </Center>
   )
 }
