719c72a8e7fd3a8988980022c390859eb2e4250d
Daniel Carvalho <daniel.devop@gmail.com>
2023-01-08 08:01:10 -0300

    adicionando um botão

--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,6 +1,7 @@
 import { Center, Text } from "native-base";
 
 import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn (){
   return (
@@ -9,6 +10,7 @@ export function SignIn (){
       bgColor='gray.900'
     >
       <Logo width={212} height={40} />  
+      <Button title="ENTRAR COM GOOGLE"/>
     </Center>
   )
 }
