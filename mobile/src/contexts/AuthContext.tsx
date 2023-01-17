fb537ce81c850e7d19cb8e075a6c86640d8cd7a4
Daniel Carvalho <daniel.devop@gmail.com>
2023-01-16 22:11:26 -0300

    controlando fluxo de isUserLoading

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import * as AuthSession from "expo-auth-session";
 import * as Google from 'expo-auth-session/providers/google'
 import * as WebBrowser from 'expo-web-browser'
@@ -12,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps
+  isUserLoading: boolean 
   signIn: () => Promise<void>
 }
 
@@ -22,20 +23,30 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({children}: AuthProviderProps){
+  const [isUserLoading, setIsUserLoading] = useState(false)
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId: 'xxx',
     redirectUri: AuthSession.makeRedirectUri({useProxy: true}), 
     scopes: ['profile','email']
-  })
+  }) 
 
   async function signIn() {
-    console.log('Vamos Logar!!!')
+    try {
+      setIsUserLoading(true)
+      await promptAsync()
+    } catch (error) {
+      console.log(error)
+      throw error;
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   return (
     <AuthContext.Provider value={{
       signIn,
+      isUserLoading,
       user: {
         name: 'UnsCrud',
         avatarUrl: 'https://github.com/unscrud.png'
