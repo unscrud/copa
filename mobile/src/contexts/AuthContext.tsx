24f36c6e4097927d7a651c8e6f3b631856840764
Daniel Carvalho <daniel.devop@gmail.com>
2023-02-17 07:53:44 -0300

    login com o google e exibindo dados no console

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as AuthSession from "expo-auth-session";
 import * as Google from 'expo-auth-session/providers/google'
 import * as WebBrowser from 'expo-web-browser'
+import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession()
 
@@ -44,7 +45,18 @@ export function AuthContextProvider({children}: AuthProviderProps){
   }
 
   async function signInWithGoogle(access_token:string) {
-    console.log("TOKEN DE AUTENTICAÇÃO ==>", access_token)
+    try {
+      setIsUserLoading(true)
+      const tokenResponse = await api.post('/users', {access_token})
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`
+      const userInfoResponse = await api.get('/me')
+      console.log(userInfoResponse.data)
+    } catch (error) {
+      console.log(error) 
+      throw error
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(()=>{
